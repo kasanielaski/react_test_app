505027d67adfced4924236166b9412f58bc65d91
Sergey Shapirenko <s.shapirenko@hsdigital.ru>
2019-02-01 16:13:16 +0700

    fix autism

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import todoApp from './reducers/Reducers';
 import './index.css';
+import store from './store';
 import App from './App';
-
-const store = createStore(todoApp);
 
 ReactDOM.render(
     <Provider store={store}>
