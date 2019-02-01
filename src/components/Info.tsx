500f1c3ff7b4da16dfe3206221d486a849eb7dd5
Sergey Shapirenko <s.shapirenko@hsdigital.ru>
2019-02-01 16:11:36 +0700

    add router

---
+++ src/components/Info.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Info = () => <h1>sup!</h1>;
+
+export default Info;
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import todoApp from './reducers/Reducers';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { store } from './store';
 import './index.css';
 import App from './App';
 
-const store = createStore(todoApp);
-
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
+    <Router>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </Router>,
     document.getElementById('root')
 );
