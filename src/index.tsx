4f9694e5de4c8a5845d303912e85f864817008b9
Sergey Shapirenko <s.shapirenko@hsdigital.ru>
2019-02-01 17:30:43 +0700

    fix component composition

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import './index.css';
 import store from './store';
-import App from './App';
+
+import ToDoList from './components/ToDoList';
+
+import './index.css';
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <ToDoList />
     </Provider>,
     document.getElementById('root')
 );
